9b37a524865277831b974bc188f356fb0b6afc72
Felipe Perlato <fe_perlato@hotmail.com>
2023-04-06 11:40:41 -0300

    primeiro commit

---
+++ Arquivos/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string caminhoorigem = @"C:\Users\Felipe\Desktop\PROJETOS\file1.txt";
+
+        string caminhodestino = @"C:\Users\Felipe\Desktop\PROJETOS\file2.txt";
+
+        try
+        {
+              FileInfo fileinfo = new FileInfo(caminhoorigem);
+
+           // fileinfo.CopyTo(caminhodestino);
+
+            string[] linhas = File.ReadAllLines(caminhoorigem);
+     
+            foreach(string linha in linhas)
+            {
+                Console.WriteLine(linha);    
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("ocorreu um erro");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
